05270c5ed7e15762d28f44e6f2765af9386e4e22
vkazandzhi <vlad_kazandzhy@ukr.net>
2020-05-04 23:24:59 -0600

    new database

--- connectDatabase.php
+++ connectDatabase.php
@@ -4,7 +4,7 @@ try
 {
   $user = 'pchqvjcytiaeip';
   $password = '87cd5e56bc35f219037b1ab9f97bf1aaef3c5aedee82780b2e75f04db4ebaa12';
-  $db = new PDO('pgsql:ec2-34-197-141-7.compute-1.amazonaws.com;dbname=dcqcvtd749slki', $user, $password);
+  $db = new PDO('pgsql:host=ec2-34-197-141-7.compute-1.amazonaws.com;dbname=dcqcvtd749slki', $user, $password);
 }
 catch (PDOException $ex)
 {
