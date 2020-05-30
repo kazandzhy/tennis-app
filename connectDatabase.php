142e132e84100e027f997cea86c670f65af764ee
vkazandzhi <vlad_kazandzhy@ukr.net>
2020-05-30 14:08:05 -0600

    New database credentials

--- connectDatabase.php
+++ connectDatabase.php
@@ -2,9 +2,9 @@
 
 try
 {
-  $user = 'pchqvjcytiaeip';
-  $password = '87cd5e56bc35f219037b1ab9f97bf1aaef3c5aedee82780b2e75f04db4ebaa12';
-  $db = new PDO('pgsql:host=ec2-34-197-141-7.compute-1.amazonaws.com;dbname=dcqcvtd749slki', $user, $password);
+  $user = 'xepidrpbrhkkrq';
+  $password = '0c7cd609fcedaa2f3d3a0fea7f8d9bbd07d172d97b8d29c534b2d59e0c1ddac4';
+  $db = new PDO('pgsql:host=ec2-18-233-32-61.compute-1.amazonaws.com;dbname=d9okmvliqi6qjn', $user, $password);
 }
 catch (PDOException $ex)
 {
